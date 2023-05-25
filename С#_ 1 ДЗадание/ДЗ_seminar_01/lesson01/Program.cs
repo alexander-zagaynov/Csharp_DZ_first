d3b50869cf10d9dd989c8103ff6e5791858ed7d1
Alexander Zagaynov <131453719+alexander-zagaynov@users.noreply.github.com>
2023-05-25 16:13:13 +0300

    Add files via upload
    Добрый день!
    
    Я не совсем понял, почему у меня так много файлов загрузилось.
    Папка с решение домашнего задания DZadanie

---
+++ С#_ 1 ДЗадание/ДЗ_seminar_01/lesson01/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*
+Найти квадрат введенного числа 
+int input = int.Parse(Console.ReadLine());
+
+Console.WriteLine(input * input);
+*/
+
+//Является ли первое число квадратом 2
+
+/*
+Console.WriteLine("Введите 2 числа ");
+int a = int.Parse(Console.ReadLine());
+
+int b = int.Parse(Console.ReadLine());
+// условия, первое число умножаем на второе и проверяем, равно ли оно второму.
+if(a * a == b){
+    Console.WriteLine("true");
+}
+else{
+    Console.WriteLine("false");
+}
+*/
+
+/*
+// выдавать название дни недели по числу
+Console.WriteLine("ведите число от 1 до 7");
+
+int number = int.Parse(Console.ReadLine());
+if (number == 1){
+    Console.WriteLine("Понедельник");
+}
+if (number == 2){
+    Console.WriteLine("Вторник");
+}
+if (number == 3){
+    Console.WriteLine("Среда");
+}
+if (number == 4){
+    Console.WriteLine("Четверг");
+}
+if (number == 5){
+    Console.WriteLine("Пятница");
+}
+if (number == 6){
+    Console.WriteLine("Суббота");
+}
+if (number == 7){
+    Console.WriteLine("Воскресенье");
+}
+else{
+    Console.WriteLine("Сегодня выходной");
+}
+*/
+
+
